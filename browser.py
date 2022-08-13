56f1c23282d6f310034ef8838d535cb2232be361
Rokon <84969208+rokonpython@users.noreply.github.com>
2022-08-14 01:14:28 +0600

    Update browser.py

--- browser.py
+++ browser.py
@@ -17,10 +17,35 @@ class MainWindow(QMainWindow):
         navbar = QToolBar()
         self.addToolBar(navbar)
 
+        #backbtn
         back_btn = QAction('Back', self)
         back_btn.triggered.connect(self.browser.back)
         navbar.addAction(back_btn)
 
+        #forwoad_btn
+        forward_btn = QAction('Forward', self)
+        forward_btn.triggered.connect(self.browser.forward)
+        navbar.addAction(forward_btn)
+
+        #reload btn
+        reload_btn = QAction('Reload', self)
+        reload_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(reload_btn)
+
+        home_btn = QAction('Home', self)
+        home_btn.triggered.connect(self.navigate_home)
+        navbar.addAction(home_btn)
+
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        navbar.addWidget(self.url_bar)
+
+        def navigate_home(self):
+            self.browser.setUrl(QUrl('https://google.com'))
+
+        def navigate_to_url(self):
+            url = self.url_bar.text()
+            self.browser.setUrl(QUrl(url))
 
 app = QApplication(sys.argv)
 QApplication.setApplicationName('My own browser')
